409ace6787759c1c2734f79fea777170b532650b
Bianca <cal22272@uvg.edu.gt>
2025-03-13 23:32:12 -0600

    feat: added code for regression model class

--- models.py
+++ models.py
@@ -13,6 +13,7 @@ from torch.nn import Parameter, Linear
 from torch import optim, tensor, tensordot, ones, matmul
 from torch.nn.functional import cross_entropy, relu, mse_loss, softmax
 from torch import movedim
+from torch import nn
 
 
 class PerceptronModel(Module):
@@ -105,66 +106,49 @@ class PerceptronModel(Module):
                         break
 
 
-class RegressionModel(Module):
-    """
-    A neural network model for approximating a function that maps from real
-    numbers to real numbers. The network should be sufficiently large to be able
-    to approximate sin(x) on the interval [-2pi, 2pi] to reasonable precision.
-    """
+
+class RegressionModel(nn.Module):
     def __init__(self):
-        # Initialize your model parameters here
-        "*** YOUR CODE HERE ***"
-        super().__init__()
-
-
+        super(RegressionModel, self).__init__()
+        # Arquitectura simple sin dropout
+        self.fc1 = nn.Linear(1, 50)
+        self.fc2 = nn.Linear(50, 50)
+        self.fc3 = nn.Linear(50, 1)
 
     def forward(self, x):
-        """
-        Runs the model for a batch of examples.
+        x = relu(self.fc1(x))
+        x = relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
 
-        Inputs:
-            x: a node with shape (batch_size x 1)
-        Returns:
-            A node with shape (batch_size x 1) containing predicted y-values
-        """
-        "*** YOUR CODE HERE ***"
-
-    
     def get_loss(self, x, y):
         """
         Computes the loss for a batch of examples.
-
+        
         Inputs:
             x: a node with shape (batch_size x 1)
             y: a node with shape (batch_size x 1), containing the true y-values
-                to be used for training
         Returns: a tensor of size 1 containing the loss
         """
-        "*** YOUR CODE HERE ***"
- 
-        
+        # Usar directamente mse_loss en lugar de nn.MSELoss()
+        return mse_loss(self(x), y)
 
     def train(self, dataset):
         """
         Trains the model.
-
-        In order to create batches, create a DataLoader object and pass in `dataset` as well as your required 
-        batch size. You can look at PerceptronModel as a guideline for how you should implement the DataLoader
-
-        Each sample in the dataloader object will be in the form {'x': features, 'label': label} where label
-        is the item we need to predict based off of its features.
-
-        Inputs:
-            dataset: a PyTorch dataset object containing data to be trained on
-            
         """
-        "*** YOUR CODE HERE ***"
-
-            
-
-
-
-
+        optimizer = optim.Adam(self.parameters(), lr=0.001)
+        dataloader = DataLoader(dataset, batch_size=50, shuffle=True)
+        
+        for epoch in range(1000):
+            for batch in dataloader:
+                x = batch['x']
+                y = batch['label']
+                
+                optimizer.zero_grad()
+                loss = self.get_loss(x, y)
+                loss.backward()
+                optimizer.step()
 
 
 
